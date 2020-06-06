6b65dff9e41e452fe09553f40a95ba9df4bb9b6d
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-06 17:28:07 +0100

    Adds user signup, login and signout using Devise and Toastr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    logger.debug "resource: #{resource}"
+    root_path
+  end
 end
